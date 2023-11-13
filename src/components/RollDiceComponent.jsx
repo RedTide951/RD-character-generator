cbe76683d5d68665896bae4a94aae3bee3eb83f6
RedTide951 <oguzhanraifbutuner@gmail.com>
2023-11-14 02:30:07 +0300

    Initial commit

--- src/components/RollDiceComponent.jsx
+++ src/components/RollDiceComponent.jsx
@@ -25,30 +25,22 @@ const RollDiceComponent = () => {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginBottom: '20px',marginTop: '20px' }}>
-      <Button variant="contained" color="primary" onClick={rollDice}>
-        Roll Stats
-      </Button>
-      {results.length > 0 && (
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-          {results.map((roll, index) => (
-            <div key={index} style={{ padding: '10px', margin: '10px', textAlign: 'center', border: '1px solid #ccc', height: '80px' }}>
-              <p>{roll.ability}</p>
-              <p>{`${roll.result}`}</p>
-            </div>
-          ))}
-        </div>
-      )}
-      {results.length > 0 && (
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-          {results.map((roll, index) => (
-            <div key={`modifier-${index}`} style={{ padding: '10px', margin: '10px', textAlign: 'center', border: '1px solid #ccc', height: '80px' }}>
-              <p>{`Modifier: ${roll.modifier > 0 ? '+' : ''}${roll.modifier}`}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <div style={{ textAlign: 'center', marginBottom: '20px', marginTop: '20px' }}>
+    <Button variant="contained" color="primary" onClick={rollDice}>
+      Roll Stats
+    </Button>
+    {results.length > 0 && (
+      <div className="dice-results">
+        {results.map((roll, index) => (
+          <div key={index} className="result-item">
+            <p>{roll.ability}</p>
+            <p>{`${roll.result}`}</p>
+            <p style={{ fontWeight: 'bold' }}>{`Modifier: ${roll.modifier > 0 ? '+' : ''}${roll.modifier}`}</p>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
   );
 };
 
